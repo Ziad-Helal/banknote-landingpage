0a86b6ac47b55b3e6f68d6bd5dcf00a7c9c7f42f
Ziad <zgalhelaly@gmail.com>
2025-09-21 12:55:18 +0300

    Not Foiund page

--- src/layouts/main-page-layout/main-page-layout.tsx
+++ src/layouts/main-page-layout/main-page-layout.tsx
@@ -1,19 +1,24 @@
-import { Suspense } from "react";
-import { Outlet } from "react-router";
+import { Suspense, useEffect } from "react";
+import { Outlet, useLocation } from "react-router";
 import { useTranslation } from "react-i18next";
 import { Toaster } from "@/components/ui";
 import { Main_PageLayout_Footer, Main_PageLayout_Header } from "./components";
 import { handleDirectionChange } from "@/localization";
+import { Loading } from "@/components/misc";
 
 export default function Main_PageLayout() {
   const { t, i18n } = useTranslation("pages");
-  const { t: tKeyWords } = useTranslation("keyWords");
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    scrollTo({ top: 0 });
+  }, [pathname]);
 
   return (
     <>
       <Main_PageLayout_Header />
-      <main className="flex-1" aria-label={t("label")}>
-        <Suspense fallback={tKeyWords("loading")}>
+      <main className="flex-1 relative" aria-label={t("label")}>
+        <Suspense fallback={<Loading isFullPage />}>
           <Outlet />
         </Suspense>
       </main>
--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/misc";
+import { Container } from "@/components/sections";
+import { useTranslation } from "react-i18next";
+
 export default function NotFound_Page() {
+  const { t } = useTranslation("pages");
+
   return (
-    <>
-      <p>NotFound_Page</p>
-    </>
+    <Container className="p-16 text-center font-[Cairo] font-medium" as="section">
+      <span className="text-destructive text-9xl">404</span>
+      <p className="mt-20 text-2xl sm:text-5xl dark:text-accent-foreground">{t("notFound.statement")}</p>
+      <Button className="mt-10" onClick={() => history.back()}>
+        {t("notFound.callToAction")}
+      </Button>
+    </Container>
   );
 }
